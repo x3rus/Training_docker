39c9cc53b39ebc34303366f78e29941626277adf
Thomas Boutry <thomas.boutry@x3rus.com>
2015-12-10 17:35:33 -0500

    Test de communication avec docker et les commandes
    Realisation de l'extraction des images disponibles sur le docker Hosts
    Realisation d'un test de build en fournissant le DockerPath
    Visualisation du resultat ... faire plus de test
    
    Voir aussi les validations mis en place sur le site :
    http://blog.bordage.pro/avoid-docker-py/
    
    pour amelioré le processus

--- kifu/build.pl
+++ kifu/build.pl
@@ -23,6 +23,19 @@ CONF_FILE="./kifu.conf" # conf file
 DONTASK=0
 
 #################
+###   FUNCs   ###
+
+def f_searchImagesName(dictImgs,searchImg):
+    for image in dictImgs:
+        if image['RepoTags'][0] == searchImg :
+            print (" Found : " , image)
+            return image
+
+    return None
+
+# FIN f_searchImagesName
+
+#################
 ####   MAIN   ###
 
 # Load configuration file
@@ -59,8 +72,28 @@ elif dontAsk_raw == 1:
 if not os.path.exists(DockerFilePATH) :
     print (" [ERROR] DockerFile Path don't exist please check it : ", DockerFilePATH)
     exit(1)
-    
 
+# Connexion a docker
+cliDocker = docker.Client(version='auto')
+
+# check if images already existe sur le systeme
+# Valider que l'on veut reBuilder l'images car ceci divergera de la version sur hub.docker.com
+dictLstImages = cliDocker.images()
+infoImage = f_searchImagesName(dictLstImages,ContainerName+":"+ContainerBaseTag)
+
+if infoImage == None:
+    print ("Are you sure you want build a new one or use already created on hub.docker.com ?")
+
+# TODO ajouter de la validation de realisation 
+ContainerNameFull=ContainerName+":"+ContainerBaseTag
+outputBuild = cliDocker.build(path=DockerFilePATH,tag=ContainerNameFull)
+for line in outputBuild:
+    print (line)
+    
+# TODO: ajouter la restriction avec DONTASK
+#if DONTASK :
+#else :
+#
     
 # read configuration variables
 #nom_container = cfg['CONTAINER_NAME']
