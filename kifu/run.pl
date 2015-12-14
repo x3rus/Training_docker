d7bab7503a51acfa868c08ac224f46f9a9f0328c
Thomas Boutry <thomas.boutry@x3rus.com>
2015-12-14 17:37:13 -0500

    suite des testes de run

--- kifu/run.pl
+++ kifu/run.pl
@@ -24,6 +24,14 @@ from configobj import ConfigObj
 ## GLOBAL VARS ##
 
 CONF_FILE="./kifu.conf" # conf file
+DONTASK=0
+ContainerID=None
+
+yes_no_valid = {"yes": True, "y": True, "ye": True, "YES": True,
+             "no": False, "n": False, "NO": False}
+
+#################
+###   FUNCs   ###
 
 def f_connect_dockerhost():
     """
@@ -37,6 +45,23 @@ def f_connect_dockerhost():
 
 # END  f_connect_dockerhost():
 
+def f_search_container_name(cliDocker,ContainerName):
+    """
+        TODO : ajout doc f_search_container_name et p-e validation try / except 
+        return (bContainerFound,ContainerID) 
+    """
+    # TODO change images pour PS method de docker
+    dictLstImages = cliDocker.images()
+    for image in dictLstImages:
+        print(image)
+#        if image['RepoTags'][0] == searchImg :
+#            print (" Found : " , image)
+#            return image
+
+    return True,"87878"
+
+# END f_search_container_name(cliDocker,ContainerName)
+
 ##################
 ####   MAIN   ####
 ##################
@@ -47,6 +72,7 @@ try :
     # TODO revoir le fichier de configuration pour des entres optionel
     ImageName=cfg['IMAGE_NAME']
     ImageBaseTag=cfg['IMAGE_BASE_TAG']
+    dontAsk_raw=cfg['DONT_ASK_USE_DEFAULT_VALUE']
 
 except KeyError:
     print (" [ERROR] you don't have define this value in your config file : ", sys.exc_info()[1])
@@ -57,6 +83,10 @@ except :
     print (" [ERROR] check the configuration file : ",CONF_FILE ) 
     exit(1)
     
+# dontAsk_validation value 
+if dontAsk_raw in yes_no_valid: 
+   DONTASK = yes_no_valid[dontAsk_raw]
+
 cliDocker = f_connect_dockerhost()
 
 ##############
@@ -77,10 +107,17 @@ cliDocker = f_connect_dockerhost()
 
 ImageNameFull=ImageName+":"+ImageBaseTag 
 ClientUsername=getpass.getuser()
+ContainerName=ClientUsername+"-Linux202_"+ImageBaseTag
+
+# TODO : faire une recherche pour le nom du container avant tentative de creation
+(bContainerFound,ContainerID) = f_search_container_name(cliDocker,ContainerName)
+
+exit(0)
+
 container_Linux202 = cliDocker.create_container(image=ImageNameFull,
                                  hostname=ClientUsername+".x3rus.com",
                                  detach=True,
-                                 name=ClientUsername+"-Linux202_"+str(datetime.date.today()),
+                                 name=ContainerName,
                              )
 start_output=cliDocker.start(container_Linux202)
 
