be78d6ae01c40d8f6c3699e100ddcc53e2fb4186
Thomas Boutry <thomas.boutry@x3rus.com>
2015-12-15 08:58:45 -0500

    Ajout d'argument pour le demarrage , ajout de la gestion si le container name existe deja pour faire un start et non une creation

--- kifu/run.pl
+++ kifu/run.pl
@@ -12,6 +12,7 @@
 ## MODULES  ##
 
 import sys
+import getopt
 import os.path
 import getpass
 import docker
@@ -25,6 +26,8 @@ from configobj import ConfigObj
 
 CONF_FILE="./kifu.conf" # conf file
 DONTASK=0
+SELECT=False
+FRESH_ONE=False
 ContainerID=None
 
 yes_no_valid = {"yes": True, "y": True, "ye": True, "YES": True,
@@ -32,6 +35,17 @@ yes_no_valid = {"yes": True, "y": True, "ye": True, "YES": True,
 
 #################
 ###   FUNCs   ###
+         
+def f_usage():
+    """
+        TODO : Ajout de doc pour f_usage
+    """
+    print ("Script to start the Docker arguments availables ")
+    print (" run.pl [-s|-f] \n")
+    print (" -s , --select : Select Container already created ")
+    print (" -f , --fresh  : start a Fresh one from the original ")
+
+# END f_usage()
 
 def f_connect_dockerhost():
     """
@@ -45,26 +59,63 @@ def f_connect_dockerhost():
 
 # END  f_connect_dockerhost():
 
-def f_search_container_name(cliDocker,ContainerName):
+def f_search_container_name(cliDocker,searchContainerName):
     """
         TODO : ajout doc f_search_container_name et p-e validation try / except 
         return (bContainerFound,ContainerID) 
     """
     # TODO change images pour PS method de docker
-    dictLstImages = cliDocker.images()
-    for image in dictLstImages:
-        print(image)
-#        if image['RepoTags'][0] == searchImg :
-#            print (" Found : " , image)
-#            return image
+    dictLstContainers=cliDocker.containers(all=True)
+    for container in dictLstContainers: 
+        # Add / to feet with the output :P
+        if container['Names'][0] == "/"+searchContainerName:
+            print (" Found : " , container)
+            return True,container
 
-    return True,"87878"
+    return True,None
 
 # END f_search_container_name(cliDocker,ContainerName)
+
+def f_ask_name_with_validation(PrefixContainerName):
+    """
+        TODO : ajout doc pour f_ask_name_with_validation
+    """
+
+    # PrefixContainerName = xerus-Linux202_base (exemple)
+    print (" Please enter the new container name , the original look like : " + PrefixContainerName + "base")
+    print (" You can change the last part _base ")
+    sys.stdout.write(" Please complet the containername : " + PrefixContainerName )
+    SufixContainerName = input().lower()
+    New_containerName = PrefixContainerName + SufixContainerName
+    
+    # TODO utiliser f_search_container_name pour voir si le nom existe deja
+    # TODO rajouter une boucle pour faire plusieurs teste si le nom existe deja .... 
+    
+# END f_ask_name_with_validation:
 
 ##################
 ####   MAIN   ####
 ##################
+
+# Get Command line arguments
+argv=sys.argv[1:]
+
+# Parse commande Line arguments
+try:
+    opts, args = getopt.getopt(argv,"hsf",["select","fresh"])
+except getopt.GetoptError:
+    f_usage()
+    exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        f_usage()
+        exit()
+    elif opt in ("-s", "--select"):
+        # argument = arg # nothing here :D
+        SELECT=True
+    elif opt in ("-f", "--fresh"):
+        FRESH_ONE=True
+
 
 # Load configuration file
 try :
@@ -89,6 +140,37 @@ if dontAsk_raw in yes_no_valid:
 
 cliDocker = f_connect_dockerhost()
 
+ImageNameFull=ImageName+":"+ImageBaseTag 
+ClientUsername=getpass.getuser()
+ContainerName=ClientUsername+"-Linux202_"+ImageBaseTag
+
+# if freshon
+if FRESH_ONE :
+    PrefixContainerName=ClientUsername+"-Linux202_"
+    ContainerName=f_ask_name_with_validation(PrefixContainerName)
+
+# TODO :  a valider si refaire une recherche est encore pertinent si on a deja fait la
+# validation dans f_ask_name_with_Validation....
+(bContainerFound,ContainerID) = f_search_container_name(cliDocker,ContainerName)
+
+if bContainerFound:
+    if DONTASK == False:
+        #TODO a ajouter
+        # ContainerName = f_ask_create_new_container_or_reuse(ContainerID)
+        raise NotImplementedError(" workin progress :P ")
+
+    start_output = cliDocker.start(ContainerID)
+else:
+    # TODO ajouter le parametre du port 22 !! 
+    container_Linux202 = cliDocker.create_container(image=ImageNameFull,
+                                 hostname=ClientUsername+".x3rus.com",
+                                 detach=True,
+                                 name=ContainerName,
+                             )
+    start_output=cliDocker.start(container_Linux202)
+
+print(start_output)
+
 ##############
 #### NOTE  ###
 # Gestion de paramètre 
@@ -105,20 +187,3 @@ cliDocker = f_connect_dockerhost()
 # 3. SNAPSHOT - autre script
 
 
-ImageNameFull=ImageName+":"+ImageBaseTag 
-ClientUsername=getpass.getuser()
-ContainerName=ClientUsername+"-Linux202_"+ImageBaseTag
-
-# TODO : faire une recherche pour le nom du container avant tentative de creation
-(bContainerFound,ContainerID) = f_search_container_name(cliDocker,ContainerName)
-
-exit(0)
-
-container_Linux202 = cliDocker.create_container(image=ImageNameFull,
-                                 hostname=ClientUsername+".x3rus.com",
-                                 detach=True,
-                                 name=ContainerName,
-                             )
-start_output=cliDocker.start(container_Linux202)
-
-print(start_output)
